5040a8754c728fbe5c87acb8fc675c0a65cd11cc
Hightwo1028 <istoo102800@gmail.com>
2024-08-01 12:18:06 +0800

    no message

--- eclipse/demo/src/tw/hightwo/js/Bike.java
+++ eclipse/demo/src/tw/hightwo/js/Bike.java
@@ -1,8 +1,12 @@
 package tw.hightwo.js;
 
-public class Bike {
+public class Bike extends Object {
 	//public > protected(子類別+相同package > 沒寫(相同package) > pravite
 	protected double speed;
+	
+	public Bike(){
+		System.out.println("Bike()");
+	}
 	
 	public void upSpeed() {
 		speed = speed < 1 ? 1 : speed * 1.4;
@@ -10,10 +14,14 @@ public class Bike {
 	
 	public void downSpeed() {
 		speed = speed < 1 ? 0 : speed * 0.7;
-
 	}
 	
 	public double getspeed() {
 		return speed;
+	}
+	
+	@Override
+	public String toString() {
+		return "I am Bike:" + speed;
 	}
 }
--- eclipse/demo/src/tw/hightwo/js/Scooter.java
+++ eclipse/demo/src/tw/hightwo/js/Scooter.java
@@ -1,11 +1,20 @@
 package tw.hightwo.js;
 //建構式是在物件初始化，不是創建物件實體
+
+//在java裡面，所有類別都一定有建構式 有 跟 定義是兩回事
 public class Scooter extends Bike {
 	private int gear;
 	private String color;
 	
-	public Scooter() {
+	public Scooter() {    //建構式
+		System.out.println("Scooter()");
 		color = "Yellow";  //在這初始化
+	}
+	
+	public Scooter(String color) {
+		// super();
+		System.out.println("Scooter(String)");
+		this.color = color;
 	}
 	
 	public void changeGear(int gear) {
