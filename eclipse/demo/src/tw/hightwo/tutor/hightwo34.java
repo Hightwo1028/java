e75b146c77395b7872efede507304c826b21e2e2
Hightwo1028 <istoo102800@gmail.com>
2024-08-06 17:56:53 +0800

    no message

---
+++ eclipse/demo/src/tw/hightwo/tutor/hightwo34.java
@@ -0,0 +1,11 @@
+package tw.hightwo.tutor;
+
+import java.io.FileInputStream;
+
+public class hightwo34 {
+
+	public static void main(String[] args) {
+		FileInputStream fin = new FileInputStream("./dir1/file1.txt");
+	}
+
+}
